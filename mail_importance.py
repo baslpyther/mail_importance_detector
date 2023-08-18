dc911c034f6f70c5d3de6939e8d73803f66660a8
basel badry <119689347+baslpyther@users.noreply.github.com>
2023-08-18 16:24:52 +0300

    Add files via upload

---
+++ mail_importance.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import joblib
+
+model=joblib.load("model.bkl")
+
+def Prediction(mail):
+    res = model.predict([mail])
+    return res[0]
+
+def Main():
+    st.title("MAIL IMPORTANCE DETECTOR")
+    mail = st.text_input("enter the mail in order to check it's importance")
+    if st.button("predict"):
+        res = Prediction(mail)
+        list_text = ["this mail is spam and might have something suspecious" , "this mail is ham and important"]
+        st.text(f"{list_text[res]}")
+st.text("model accuracy score is 98%-Bernoulli Naive_Baye ")
+Main()
+
